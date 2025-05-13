22951cd7943d104bd232e98bee318f892fd2ef54
Brian Hill <bhill@altair.com>
2025-05-13 15:55:14 -0400

    Added optional UVM version

--- sim/accel.bat
+++ sim/accel.bat
@@ -1,10 +1,13 @@
 @echo off
+set uvm=%1
+IF "%uvm%"=="" SET uvm=1.2
+
 dvlcom -F ../rtl/icache/icache.f
-dvlcom -uvm 1.2 -F ../tb/dram_ctrl/dram_ctrl.f
+dvlcom -uvm %uvm% -F ../tb/dram_ctrl/dram_ctrl.f
 dvlcom ../tb/icache/tb_ic_top.sv
-dvlcom -uvm 1.2 -F ../tb/icache/tb_icache.f
+dvlcom -uvm %uvm% -F ../tb/icache/tb_icache.f
 dvlcom ../rtl/top/dram_arb.sv
 dvlcom ../tb/icache/ic_ctrl_cover.sv
 
-dsim -top work.tb_ic_top -genimage image -separate-unit-scopes  -uvm 1.2 +acc+b -L gowin -j 8
-dsim -image image -uvm 1.2 -waves ic.mxd +UVM_TESTNAME=ic_stress_test
+dsim -top work.tb_ic_top -genimage image -separate-unit-scopes  -uvm %uvm% +acc+b -L gowin -j 8
+dsim -image image -uvm %uvm% -waves ic.mxd +UVM_TESTNAME=ic_stress_test
